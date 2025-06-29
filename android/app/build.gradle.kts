e6e1f84e5bcff87e73a54ea09554e293d232a685
Hector Zambrano <zambranohector2002@gmail.com>
2025-06-29 18:08:06 -0500

    <fix>[change the version fo the ndkversion]: <description>
    ndkVersion = "27.0.12077973" // Updated for dynamic_color plugin compatibility
    [optional body]
    
    [optional footer(s)]

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.mood_notes"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // Updated for dynamic_color plugin compatibility
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
